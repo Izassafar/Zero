9a0c323743d17cb39a8c81c8c57a943a0c11b13c
Izassafar <ahmedizas77780@gmail.com>
2017-09-05 15:49:52 +0530

    Create testcase.c

---
+++ testcase.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("Enter the number");
+scanf("%d",&number);
+if(number>=1)
+{
+printf("The number is Positive Testcase");
+}
+else
+{
+printf("The number is Negative Testcase");
+}
+}
