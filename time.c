73daed8753a30ea856157ddf28a1d8d1163a1d6e
Izassafar <ahmedizas77780@gmail.com>
2018-01-09 17:02:31 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int min,hrs;
+	printf("Enter the Minutes :");
+	scanf("%d",&min);
+	if(min<60)
+	{
+		min=min;
+	}
+	else
+	{
+		min=min%60;
+		hrs=min/60;
+	}
+	printf("%d\t%d",hrs,min);
+}
