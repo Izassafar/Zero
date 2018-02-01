7f45a3f303bbecb81ff57553915179ea0cdd668b
Izassafar <ahmedizas77780@gmail.com>
2018-02-01 14:17:45 +0530

    Create printonlynum.c

---
+++ printonlynum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s[20],p[50];
+    int i,j;
+    printf("Enter the String:");
+    scanf("%s",&s);
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(isdigit(s[i]))
+        {
+            p[j]=s[i];
+            j++;
+        }
+    }
+    printf("%s",p);
+    return 0;
+}
