b04a23946fa34308efe3c3494382ef79ead01717
Izassafar <ahmedizas77780@gmail.com>
2018-02-09 14:46:35 +0530

    Create Exist.c

---
+++ Exist.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,i,a[20];
+    printf("Enter the Size:");
+    scanf("%d",&n);
+    printf("\nEnter Search element:");
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]==k)
+        {
+            printf("\nYes");
+        }
+    }
+}
