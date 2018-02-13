85273ddf6cffe4b27724a63bcf344ec93227f644
Izassafar <ahmedizas77780@gmail.com>
2018-02-13 16:14:43 +0530

    Create Area of Triangle.c

---
+++ Player/Area of Triangle.c
@@ -0,0 +1,9 @@
+#include<stdio
+void main()
+{
+	int b,h,area;
+	printf("enter the Base and height of Triangle:");
+	scanf("%d%d",&b,&h);
+	area=(b*h)/2;
+	printf("\nThe Area Of triangle is %d",area);	
+}
