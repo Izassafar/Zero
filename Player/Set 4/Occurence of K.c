478a1e94e0ac33f0dec6aff8cb707ee27fbdcb4f
Izassafar <ahmedizas77780@gmail.com>
2018-03-02 14:14:03 +0530

    Create Occurence of K.c

---
+++ Player/Set 4/Occurence of K.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int r,n,k,i,count=0;
+	printf("Enter the  Number:");
+	scanf("%d",&n);
+	printf("Enter Kth Element:");
+	scanf("%d",&k);
+	while(n!=0)
+	{
+		r=n%10;
+		if(r==k)
+		{
+			count++;
+		}
+		n=n/10;
+	}
+	printf("%d",count);
+}
