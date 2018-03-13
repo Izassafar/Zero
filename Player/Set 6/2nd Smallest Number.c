0250f16e75d28e4785aff0410c99b36608302d5d
Izassafar <ahmedizas77780@gmail.com>
2018-03-13 16:42:57 +0530

    Update and rename Second Smallest.c to 2nd Smallest Number.c

--- Player/Set 6/2nd Smallest Number.c
+++ Player/Set 6/2nd Smallest Number.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-	int i,j,n,t,a[50];
+	int i,j,n,temp,s[50];
 	printf("Enter the Size:");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d",&s[i]);
 	}
 	for(i=0;i<n;i++)
 	{
@@ -14,13 +14,13 @@ void main()
 		{
 			if(a[i]>a[j])
 			{
-				t=a[i];
+				temp=a[i];
 			    a[i]=a[j];
-			    a[j]=t;
+			    a[j]=temp;
 			}
 		}
 	}
-	printf("%d",a[1]);
+	printf("%d",s[1]);
 
 	
 }
