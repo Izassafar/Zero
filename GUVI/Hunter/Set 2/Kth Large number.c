518af0e5f182e20b7e5bd6a0c357586e1acdd744
Izassafar <ahmedizas77780@gmail.com>
2018-09-13 03:11:48 -0700

    Create Kth Large number.c

---
+++ GUVI/Hunter/Set 2/Kth Large number.c
@@ -0,0 +1,25 @@
+//Print Kth Largest element
+
+#include<stdio.h>
+int main()
+{
+    int n,k,a[50],i,j,t;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    printf("%d",a[n-k]);
+}
